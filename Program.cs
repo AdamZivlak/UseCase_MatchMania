59990aaf426e0c4de96c858a5178b0386a5f60a8
Adam Zivlak <adam.zivlak@gmail.com>
2023-10-02 14:38:20 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UseCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Repo för att pusha/pulla Use Cases");
+        }
+    }
+}
